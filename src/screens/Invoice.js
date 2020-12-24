22944eee4b1fde817b3b1318c13e89aa066c9789
Felipe Romo <felipe@fromo.me>
2020-12-24 12:36:40 -0600

    scaffold skeleton for invoice

--- src/screens/Invoice.js
+++ src/screens/Invoice.js
@@ -1,3 +1,40 @@
 import React from 'react';
 
-export const InvoiceScreen = (_) => <div>Invoice Screen</div>;
+import { Avatar } from '../components/Avatar';
+import { Copy } from '../components/Typography';
+import { Tile } from '../components/Tile';
+import { Button } from '../components/Button';
+import { Card } from '../components/Card';
+
+export const InvoiceScreen = (_) => (
+  <div>
+    <div>
+      <nav>
+        <button>hamburger</button>
+        <Avatar />
+      </nav>
+      <div>
+        <Copy child="Send To" />
+        <Tile />
+      </div>
+      <div>
+        <div>
+          <Copy child="Invoice Details" />
+          <Button buttonHierarchy="primary" child="Add New Task" />
+        </div>
+        <div>
+          <Card />
+          <Card />
+        </div>
+        <div>
+          <Copy child="Amount Payable" />
+          <Copy child="$250.24 USD" />
+        </div>
+      </div>
+      <div>
+        <Button buttonHierarchy="primary" child="Download Project" />
+        <Button buttonHierarchy="secondary" child="Send Details" />
+      </div>
+    </div>
+  </div>
+);
