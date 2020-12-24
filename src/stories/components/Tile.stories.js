6dbeaee051e14b4f70b4bda3b5b80facf2da126d
Felipe Romo <felipe@fromo.me>
2020-12-24 11:49:48 -0600

    build default tile story and wrap stories in padding decorator

--- src/stories/components/Tile.stories.js
+++ src/stories/components/Tile.stories.js
@@ -5,11 +5,28 @@ import { Tile } from '../../components/Tile';
 const METADATA = {
   title: 'Component Library/Tile',
   component: Tile,
+  decorators: [story => <div style={{ padding: '3rem'}}>{story()}</div>],
 };
 
 export default METADATA;
 
 const Template = (args) => <Tile {...args} />;
 
+export const DefaultTile = Template.bind({});
+DefaultTile.parameters = {
+  backgrounds: { default: 'dark'},
+};
+DefaultTile.args = {
+  title: 'Brandbook.PDF',
+  subtitle: 'Dropbox/Project/El Brandbook',
+  action: {
+    child: 'Send PDF',
+    onPress: null,
+  }
+};
+
 export const NoArgs = Template.bind({});
+NoArgs.parameters = {
+  backgrounds: { default: 'dark'},
+};
 NoArgs.args = {};
