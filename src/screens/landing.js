3ebbc64a001d463ac914357410e592251d9b1b93
Felipe Romo <felipe@fromo.me>
2020-12-24 12:57:53 -0600

    pass props to sections and text components

--- src/screens/landing.js
+++ src/screens/landing.js
@@ -9,10 +9,10 @@ export const LandingScreen = (_) => (
   <div className="landing-section">
     <div>
       <img />
-      <Heading child="A Better Way To Manage Your Team"/>
-      <SubHeading child="Manage team tasks and projects with one platform and keep in touch with your team"/>
+      <Heading child="A Better Way To Manage Your Team" color="dark" style={{ fontSize: '30px'}}/>
+      <SubHeading child="Manage team tasks and projects with one platform and keep in touch with your team" color="secondary" style={{ fontSize: '17px'}} />
     </div>
-    <div>
+    <div className="landing button-row">
       <Button buttonHierarchy="tertiary" child="Skip"/>
       <Button buttonHierarchy="primary" child="Get started"/>
     </div>
