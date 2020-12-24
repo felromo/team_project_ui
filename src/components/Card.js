3f54144de0c1d5d15ef6dd4b7cfe80485184a9c2
Felipe Romo <felipe@fromo.me>
2020-12-24 06:15:20 -0600

    add proptypes and rename props accept an action button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { SubHeading, Copy } from './Typography';
+import { Button } from './Button';
 
 import './card.css';
 
-export const Card = ({ child }) => (
+export const Card = ({ title, amount, status, action }) => (
   <div class="brand-card">
     <Copy color="light" child="Meetings" />
-    <SubHeading color="light" child="10" />
-    <Copy color="light" child="Pending" />
+    <div>
+      <SubHeading color="light" child="10" />
+      <Copy color="light" child="Pending" />
+      {action}
+    </div>
   </div>
 );
+
+Card.propTypes = {
+  title: PropTypes.string,
+  amount: PropTypes.number,
+  status: PropTypes.string,
+  action: PropTypes.instanceOf(Button),
+};
+Card.defaultProps = {
+  title: 'Provide a title',
+  amount: 0,
+  status: 'Provide a status',
+  action: null,
+};
