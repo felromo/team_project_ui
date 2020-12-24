7f748457c82d32f51aae9b862c82a89d4851af32
Felipe Romo <felipe@fromo.me>
2020-12-24 07:49:34 -0600

    include css and change html structure

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './progressbar.css';
+
 export const ProgressBar = (_) => (
   <div className="progressbar-wrapper">
-    <div className="progressbar-bar" />
-    <div className="progressbar-progress" />
+    <div className="progressbar-bar">
+      <div className="progressbar-progress" />
+    </div>
   </div>
 );
