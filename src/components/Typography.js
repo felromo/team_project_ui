71cf65f7f8fda0071bfadf4a80b2faa4d4446faf
Felipe Romo <felipe@fromo.me>
2020-12-24 06:01:46 -0600

    allow for typography to accept a passed in color

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -3,53 +3,51 @@ import PropTypes from 'prop-types';
 
 import './typography.css';
 
-export const Heading = ({ child }) => (
-  <p className="brand-heading">
-    {child}
-  </p>
+export const Heading = ({ child, color }) => (
+  <p className={`brand-heading ${color ? color + '-color' : ''}`}>{child}</p>
 );
 
 Heading.propTypes = {
   child: PropTypes.string,
+  color: PropTypes.string,
 };
 Heading.defaultProps = {
   child: 'Child is the text to be displayed',
+  color: 'primary',
 };
 
-export const SubHeading = ({ child }) => (
-  <p
-    className="brand-subheading"
-  >
-    {child}
-  </p>
+export const SubHeading = ({ child, color }) => (
+  <p className={`brand-subheading ${color ? color + '-color' : ''}`}>{child}</p>
 );
 SubHeading.propTypes = {
   child: PropTypes.string,
+  color: PropTypes.string,
 };
 SubHeading.defaultProps = {
   child: 'Child is the text to be displayed',
+  color: 'primary',
 };
 
-export const Title = ({ child }) => (
-  <p className="brand-title">
-    {child}
-  </p>
+export const Title = ({ child, color }) => (
+  <p className={`brand-title ${color ? color + '-color' : ''}`}>{child}</p>
 );
 Title.propTypes = {
   child: PropTypes.string,
+  color: PropTypes.string,
 };
 Title.defaultProps = {
   child: 'Child is the text to be displayed',
+  color: 'primary',
 };
 
-export const Copy = ({ child }) => (
-  <p className="brand-copy">
-    {child}
-  </p>
+export const Copy = ({ child, color }) => (
+  <p className={`brand-copy ${color ? color + '-color' : ''}`}>{child}</p>
 );
 Copy.propTypes = {
   child: PropTypes.string,
+  color: PropTypes.string,
 };
 Copy.defaultProps = {
   child: 'Child is the text to be displayed',
+  color: 'primary',
 };
