0c1521eac4abd50cfd41369a8db3c0ce52457a9f
Felipe Romo <felipe@fromo.me>
2020-12-24 07:42:53 -0600

    create the skeleton

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const ProgressBar = (_) => <div>ProgressBar</div>;
+export const ProgressBar = (_) => (
+  <div className="progressbar-wrapper">
+    <div className="progressbar-bar" />
+    <div className="progressbar-progress" />
+  </div>
+);
