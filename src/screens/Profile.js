43489798180b0e70c179822f3c5e56062764ea32
Felipe Romo <felipe@fromo.me>
2020-12-24 14:54:55 -0600

    pass in props

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -11,7 +11,9 @@ export const ProfileScreen = (_) => (
   <div class="profile-section">
     <div>
       <nav>
-        <button><div className="hamburger" /></button>
+        <button>
+          <div className="hamburger" />
+        </button>
         <Avatar />
       </nav>
       <div className="title-section">
@@ -20,16 +22,24 @@ export const ProfileScreen = (_) => (
           child="Good Evening User!"
           style={{ fontSize: '20px' }}
         />
-        <div style={{height: '8px'}}/>
+        <div style={{ height: '8px' }} />
         <Heading color="dark" child="Projects" />
       </div>
       <div className="grid-section">
         <Title child="Overviews" />
         <div className="grid-container">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card title="Meetings" amount={10} status="pending" />
+          <Card
+            light
+            title="Projects"
+            amount={5}
+            status="ongoing"
+            action={{ child: 'Review Project', onPress: null }}
+          />
+          <Card light title="Meetings" amount={10} status="pending" 
+            action={{ child: 'Schedule Meeting', onPress: null }}
+          />
+          <Card light title="Revenue" amount={254.1} status='' progress={{ label: '+25.21%', value: 25}}/>
         </div>
       </div>
       <div className="bot-buttons-section">
