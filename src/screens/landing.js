61cafbc9c5616c8e09469dbbb01cbb7dcc490116
Felipe Romo <felipe@fromo.me>
2020-12-24 12:25:31 -0600

    add skeleton of screen

--- src/screens/landing.js
+++ src/screens/landing.js
@@ -1,3 +1,18 @@
 import React from 'react';
 
-export const LandingScreen = (_) => <div>landing screen</div>;
+import { Heading, SubHeading } from '../components/Typography';
+import { Button } from '../components/Button';
+
+export const LandingScreen = (_) => (
+  <div>
+    <div>
+      <img />
+      <Heading child="A Better Way To Manage Your Team"/>
+      <SubHeading child="Manage team tasks and projects with one platform and keep in touch with your team"/>
+    </div>
+    <div>
+      <Button buttonHierarchy="tertiary" child="Skip"/>
+      <Button buttonHierarchy="primary" child="Get started"/>
+    </div>
+  </div>
+)
