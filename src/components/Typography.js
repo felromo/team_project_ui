7fa9c3648f69425447370c9eea9f6b9449a08dcf
Felipe Romo <felipe@fromo.me>
2020-12-24 05:02:52 -0600

    add other bodies of text that will be used

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -15,3 +15,39 @@ Heading.propTypes = {
 Heading.defaultProps = {
   child: 'Child is the text to be displayed',
 };
+
+export const SubHeading = ({ child }) => (
+  <p>
+    {child}
+  </p>
+);
+SubHeading.propTypes = {
+  child: PropTypes.string,
+};
+SubHeading.defaultProps = {
+  child: 'Child is the text to be displayed',
+};
+
+export const Title = ({ child }) => (
+  <p>
+    {child}
+  </p>
+);
+Title.propTypes = {
+  child: PropTypes.string,
+};
+Title.defaultProps = {
+  child: 'Child is the text to be displayed',
+};
+
+export const Copy = ({ child }) => (
+  <p>
+    {child}
+  </p>
+);
+Copy.propTypes = {
+  child: PropTypes.string,
+};
+Copy.defaultProps = {
+  child: 'Child is the text to be displayed',
+};
