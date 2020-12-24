8582525289294ef6353c70a8ec33e8ad16efe30a
Felipe Romo <felipe@fromo.me>
2020-12-24 12:36:49 -0600

    scaffold skeleton for profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,3 +1,33 @@
 import React from 'react';
 
-export const ProfileScreen = (_) => <div>Profile Screen</div>;
+import { Avatar } from '../components/Avatar';
+import { Heading, SubHeading, Title } from '../components/Typography';
+import { Card } from '../components/Card';
+import { Button } from '../components/Button';
+
+export const ProfileScreen = (_) => (
+  <div>
+    <div>
+      <nav>
+        <button>hamburger</button>
+        <Avatar />
+      </nav>
+      <div>
+        <SubHeading child="Good Evening User!" />
+        <Heading child="Projects" />
+      </div>
+      <div>
+        <Title child="Overviews" />
+        <div>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+      <div>
+        <Button buttonHierarchy="primary" child="Create Project" />
+      </div>
+    </div>
+  </div>
+);
