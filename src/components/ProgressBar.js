fb2b5e2d887defeb479272baafea4f274f0ea124
Felipe Romo <felipe@fromo.me>
2020-12-24 08:25:36 -0600

    accept value prop, display progress based on value

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -3,10 +3,17 @@ import PropTypes from 'prop-types';
 
 import './progressbar.css';
 
-export const ProgressBar = (_) => (
+export const ProgressBar = ({ value }) => (
   <div className="progressbar-wrapper">
     <div className="progressbar-bar">
-      <div className="progressbar-progress" />
+      <div className="progressbar-progress" style={{ width: `${140 * (value/100)}px`}}/>
     </div>
   </div>
 );
+
+ProgressBar.propTypes = {
+  value: PropTypes.number,
+};
+ProgressBar.defaultProps = {
+  value: 0,
+};
