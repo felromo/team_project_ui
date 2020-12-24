41725d90ddf406abf582dd1c0c1f24b7b8630aee
Felipe Romo <felipe@fromo.me>
2020-12-24 08:37:39 -0600

    set proptypes and default props

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 
 import './avatar.css';
 
-export const Avatar = (_) => (
-  <div>
-    <img
-      src="https://randomuser.me/api/portraits/women/63.jpg"
-      alt="user avatar"
-    />
-    <div></div>
+export const Avatar = ({ image, alt, status }) => (
+  <div className="avatar-wrapper">
+    <img src={image} alt={alt} />
+    <div className={`avatar-status-indicator ${status}-status`}></div>
   </div>
 );
 
-Avatar.propTypes = {};
-Avatar.defaultProps = {};
+Avatar.propTypes = {
+  image: PropTypes.string,
+  alt: PropTypes.string,
+  status: PropTypes.string,
+};
+Avatar.defaultProps = {
+  image: 'https://randomuser.me/api/portraits/women/63.jpg',
+  alt: 'user avatar',
+  status: 'online',
+};
