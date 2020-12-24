d26fefe8f82f535f94ef62b2636b5a5088a3ae05
Felipe Romo <felipe@fromo.me>
2020-12-24 08:34:15 -0600

    add image and html skeleton

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,7 +3,15 @@ import PropTypes from 'prop-types';
 
 import './avatar.css';
 
-export const Avatar = (_) => <div>Avatar Component</div>;
+export const Avatar = (_) => (
+  <div>
+    <img
+      src="https://randomuser.me/api/portraits/women/63.jpg"
+      alt="user avatar"
+    />
+    <div></div>
+  </div>
+);
 
 Avatar.propTypes = {};
 Avatar.defaultProps = {};
